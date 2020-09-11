7693dcc7a56d9940b25dd1838f9827f361904a95
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2020-09-11 16:08:49 +0300

    first

---
+++ application.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+from tensor_algo import cosine_distance_with_tensors
+from glove import compare
+
+application = Flask(__name__)
+api = Api(application)
+
+parser = reqparse.RequestParser()
+parser.add_argument("answer1")
+parser.add_argument("answer2")
+
+class Index(Resource):
+    def get(self):
+        return {"start": "page"}
+
+class Checker(Resource):
+    def get(self):
+        return {"goal": "compare strings"}
+
+    def post(self):
+        args = parser.parse_args()
+        answer = str(args.answer1)
+        sample = str(args.answer2)
+        answer_split = answer.split(" ")
+        filter(lambda x: x != "," or x != "." or x != ":", answer_split)
+        sample_split = sample.split(" ")
+        filter(lambda x: x != "," or x != "." or x != ":", sample_split)
+        if len(answer_split) <= 3 or len(sample_split) <= 3:
+            res = compare(answer, sample)
+        else:
+            res = cosine_distance_with_tensors(answer, sample)
+
+        comment = 0
+
+        # res = cosine_distance_with_tensors(answer, sample)
+
+
+        if res < 70:
+            if len(sample_split) / len(answer_split) > 2:
+                comment = "Дайте более развернутый ответ"
+            elif len(answer_split) / len(sample_split) > 2:
+                comment = "Дайте более короткий ответ"
+
+        return {"res": str(res), "comment": comment}
+
+
+##
+# Actually setup the Api resource routing here
+##
+api.add_resource(Index, "/")
+api.add_resource(Checker, "/checker")
+
+if __name__ == "__main__":
+    application.run(debug=True)
---
+++ tensor_algo.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+
+client = boto3.client("sagemaker-runtime", region_name="us-east-1")
+
+
+def vectorize(sentence):
+    endpoint_name = (
+        "tensorflow-inference-2020-09-09-09-32-37-203"  # Your endpoint name.
+    )
+    content_type = (
+        "application/json"  # The MIME type of the input data in the request body.
+    )
+    accept = "application/json"
+    payload = sentence
+    payload = json.dumps(payload)
+    response = client.invoke_endpoint(
+        EndpointName=endpoint_name,
+        ContentType=content_type,
+        Accept=accept,
+        Body=payload,
+    )
+    res = json.loads((response["Body"].read().decode()))
+    return res["predictions"][0]
+
+def cosine_distance_with_tensors(s1, s2):
+
+    from scipy.spatial import distance
+    text_to_vector_v1 = vectorize(s1)
+    text_to_vector_v2 = vectorize(s2)
+    cosine = distance.cosine(text_to_vector_v1, text_to_vector_v2)
+    return round((1 - cosine) * 100, 2)
