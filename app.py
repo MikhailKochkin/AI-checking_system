614d7ff2770c1ef24a0c66b6042a7b180b42a931
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2023-10-29 18:35:09 +0400

    attempt1

--- app.py
+++ app.py
@@ -246,4 +246,5 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
-    app.run()
+    app.run(host='localhost',port=8000, debug=True)
+
